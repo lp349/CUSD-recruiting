6145d32fef5723c121bc83711ec02e2829b12887
Linda Pei <lp349@cornell.edu>
2015-10-03 14:07:47 -0400

    Fixed title display

--- joincusd/mainsite/views.py
+++ joincusd/mainsite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def index(request):
-  return render_to_response('index.html')
+  return render_to_response('index.html', {'title': 'Cornell University Sustainable Design'})
