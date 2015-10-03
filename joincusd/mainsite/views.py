aee30ccaae5d1170bdd3eaef85588d7921c4742f
Linda Pei <lp349@cornell.edu>
2015-10-03 14:20:12 -0400

    added project file

--- joincusd/mainsite/views.py
+++ joincusd/mainsite/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 def index(request):
   return render_to_response('index.html', {'title': 'Cornell University Sustainable Design'})
+
+def projects(request):
+  return render_to_response('projects.html', {'title': 'Projects'})
