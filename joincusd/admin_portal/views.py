eb448096ec0d902096837c470483b5233e1f5f7a
Willy <willy824@gmail.com>
2015-11-01 10:00:08 -0500

    roles list + separate ajax link for roles

--- joincusd/admin_portal/views.py
+++ joincusd/admin_portal/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from mainsite.models import Posting
+from mainsite.models import Posting, Opening
 
 import json
 
@@ -33,6 +33,7 @@ def index(request):
 # returns:
 #   a JSON object representing the following Python object
 #   [Posting object, Posting object...]
+#   with the name, id, and posting_type fields exposed
 def posting_list(request, posting_type):
 
      postings = Posting.objects.filter(posting_type=posting_type)
@@ -45,6 +46,26 @@ def posting_list(request, posting_type):
           post_object["id"] = post.id
 
           result_list.append(post_object)
+
+     response = json.dumps(result_list)
+     return HttpResponse(response)
+
+# arguments:
+#   none except required HttpRequest
+#
+# returns:
+#   a JSON object representing the following Python object
+#   [Opening object, Opening object...]
+#   with title, description fields exposed
+def role_list(request):
+     roles = Opening.objects.all()
+     result_list = []
+
+     for role in roles:
+         role_object = {}
+         role_object["title"] = role.title
+         role_object["description"] = role.description
+         result_list.append(role_object)
 
      response = json.dumps(result_list)
      return HttpResponse(response)
