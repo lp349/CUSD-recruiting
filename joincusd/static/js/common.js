a1304b6e6a744c871b9814d9b07b644a0dcc23c1
minhuig2394 <mg797@cornell.edu>
2015-11-01 17:32:18 -0500

    fixed logo not displaying on login

--- joincusd/static/js/common.js
+++ joincusd/static/js/common.js
@@ -6,7 +6,7 @@ $(window).resize( function() {
     } else $(".site-logo__text").show();
 });
 
-$(window).load(function() {
+$(window.document).load(function() {
     if (window.outerWidth < 920) {
         $(".site-logo__text").hide();
     } else $(".site-logo__text").show();
