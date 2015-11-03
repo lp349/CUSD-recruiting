6ae53e97a6882c6af5b4ddf3ad5d7850658f40d4
Linda Pei <lp349@cornell.edu>
2015-11-02 19:55:00 -0500

    changed urls

--- joincusd/admin_portal/urls.py
+++ joincusd/admin_portal/urls.py
@@ -6,6 +6,18 @@ urlpatterns = [
     url(r'^login/$', views.admin_login),
     url(r'^ajax/(?P<posting_type>[a-z_]+)/$', views.posting_list),
     url(r'^ajax/roles$', views.role_list),
-    url(r'^add_project$', views.project_form_handler),
-    url(r'^role$', views.role)]
+    url(r'^add_project/$', views.project_form_handler),
+    # url(r'^add_role_type/$', views.project_form_handler),
+    # edit project/role_type url
+    # url(r'^edit_project/(?P<pk>[0-9]+)$',)
+    # url(r'^edit_role_type/(?P<pk>[0-9]+)$',)
+    # remove project/role_type url
+    # url(r'^remove_project/(?P<pk>[0-9]+)$',)
+    # url(r'^remove_role_type/(?P<pk>[0-9]+)$',)
+    url(r'^add_role/$', views.role)
+    # edit role
+    # url(r'^edit_role/(?P<pk>[0-9]+)$',)
+    # remove role
+    # url(r'^remove_role/(?P<pk>[0-9]+)$',)
+]
 
