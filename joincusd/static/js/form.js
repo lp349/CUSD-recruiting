b2f9f193768cee0cc903218059d5751d26815911
minhuig2394 <mg797@cornell.edu>
2015-11-03 14:19:00 -0500

    some more css changes

--- joincusd/static/js/form.js
+++ joincusd/static/js/form.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    //$("input:file").addClass("button");
     $("li").on("click", "label", function (e) {
         // Toggle the checkbox
         var wrapper = $(this);
