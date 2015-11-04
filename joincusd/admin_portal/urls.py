42928677c21ee64423d4962124084458b5ce781a
minhuig2394 <mg797@cornell.edu>
2015-11-04 00:30:56 -0500

    role and url changes

--- joincusd/admin_portal/urls.py
+++ joincusd/admin_portal/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
     url(r'^ajax/(?P<posting_type>[a-z_]+)/$', views.posting_list),
     url(r'^ajax/roles$', views.role_list),
     url(r'^add_project/$', views.posting_form_handler, {'posting_type':'project','is_edit':False,'pk':None}),
-    # url(r'^add_role_type/$', views.project_form_handler),
+    url(r'^add_role_type/$', views.posting_form_handler, {'posting_type':'role_type','is_edit':False,'pk':None}),
     # edit project/role_type url
     url(r'^edit_project/(?P<pk>[0-9]+)/$', views.posting_form_handler, {'posting_type':'project', 'is_edit':True}),
-    # url(r'^edit_role_type/(?P<pk>[0-9]+)/$',)
+    url(r'^edit_role_type/(?P<pk>[0-9]+)/$', views.posting_form_handler, {'posting_type':'role_type', 'is_edit':True}),
     # remove project/role_type url
     # url(r'^remove_project/(?P<pk>[0-9]+)/$',)
     # url(r'^remove_role_type/(?P<pk>[0-9]+)/$',)
     
-    url(r'^add_role/(?P<pk>[0-9]+)/$', views.role),
+    #url(r'^add_role/(?P<pk>[0-9]+)/$', views.role),
     url(r'^add_role/$', views.role, {'pk':None}),
     # edit role
     #url(r'^edit_role/(?P<pk>[0-9]+)/$',views.edit_role),
