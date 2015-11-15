ef22d60ffd5824b6b2467546871433349fbbb1ca
Yujia Liu <yl2254@cornell.edu>
2015-11-15 14:54:42 -0500

    fix application upload file

--- joincusd/application/forms.py
+++ joincusd/application/forms.py
@@ -1,19 +1,19 @@
 from django import forms
 from mainsite.models import Application, Posting, Opening
+MAX_UPLOAD_SIZE= 1048576
 
 class ApplicationForm(forms.ModelForm):
     #this is hardcoded in the project or role type page templates
     #posting_type = forms.IntegerField(widget=forms.HiddenInput(), initial="project")
     netID = forms.CharField(max_length=10, help_text="Your Cornell netID:")
     
-    resume = forms.FileField(help_text="Resume Upload:")
+    resume = forms.FileField(help_text="Resume Upload:", max_length=MAX_UPLOAD_SIZE, allow_empty_file=False, validators=[validate_file_extensions])
     
     all_role_choices = [(role.pk, role.title) for role in Opening.objects.all()]
     role_multiselect = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, help_text="Select Roles", choices = all_role_choices, required=False)
 
     all_projects_choices = [(project.pk, project.name) for project in Posting.objects.filter(posting_type="project")]
-    print('forms!!!!!')
-    print(all_projects_choices)
+
     project_multiselect = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, help_text="Select Projects", choices = all_projects_choices, required=False)
 
     '''def clean(self):
@@ -38,3 +38,6 @@ class ApplicationForm(forms.ModelForm):
         model = Application
         fields = ('netID', 'resume', 'role_multiselect', 'project_multiselect')
 
+def validate_file_extensions(value):
+  if not (value.name.endswith('.pdf') or value.name.endswith('.doc') or value.name.endswith('.docx')):
+    raise ValidationError(u'Invalid file extension')
