1862d76a62a02908ca2ca62e205ba0197ef8bd8d
minhuig2394 <mg797@cornell.edu>
2015-11-15 21:30:00 -0500

    alphabetical role types
    …for consistency

--- joincusd/static/js/list.js
+++ joincusd/static/js/list.js
@@ -389,6 +389,13 @@ var display_project_list = function (data) {
  */
 var display_roletype_list = function (data) {
     var posts = JSON.parse(data);
+    
+    //sort alphabetically before display
+    posts.sort(function (post1, post2) {
+        if (post1.name < post2.name) return -1;
+        return post1.name > post2.name;
+    });
+
     var list_div = $("#content");
     list_div.empty();
 
