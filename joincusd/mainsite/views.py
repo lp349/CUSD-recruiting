d4c50fa6c52a46b63fff99b7ad6f81db4b12f8ad
Linda Pei <lp349@cornell.edu>
2015-11-30 13:19:14 -0500

    spplit up main page to diff sections

--- joincusd/mainsite/views.py
+++ joincusd/mainsite/views.py
@@ -3,35 +3,41 @@ from .models import Posting, Opening
 
 # Create your views here.
 def index(request):
+  projects = Posting.objects.filter(posting_type='project', published=True).order_by('-rank')
   context = {
     'title': 'Cornell University Sustainable Design',
-    'projects': Posting.objects.filter(posting_type='project'),
-    'role_types': Posting.objects.filter(posting_type='role_type')
+    'projects': projects,
+    'role_types': Posting.objects.filter(posting_type='role_type', published=True),
   }
   return render_to_response('index.html', context)
 
 def projects(request):
+  projects = Posting.objects.filter(posting_type='project', published=True).order_by('-rank')
+  display_projects = [projects[x:x+3] for x in range(0, len(projects), 3)]
   context = {
     'title': 'Projects',
-    'projects': Posting.objects.filter(posting_type='project').order_by('-rank'),
-    'role_types': Posting.objects.filter(posting_type='role_type')
+    'display_projects': display_projects,
+    'projects': projects,
+    'role_types': Posting.objects.filter(posting_type='role_type', published=True)
   }
   return render_to_response('projects.html', context)
 
 def about(request):
+  projects = Posting.objects.filter(posting_type='project', published=True).order_by('-rank')
   context = {
     'title': 'About',
-    'projects': Posting.objects.filter(posting_type='project'),
-    'role_types': Posting.objects.filter(posting_type='role_type')
+    'projects': projects,
+    'role_types': Posting.objects.filter(posting_type='role_type', published=True)
   }
   return render_to_response('about.html', context)
 
 def posting(request, pk):
   posting = Posting.objects.get(pk=pk)
+  projects = Posting.objects.filter(posting_type='project', published=True).order_by('-rank')
   context = {
     'title': 'About',
-    'projects': Posting.objects.filter(posting_type='project'),
-    'role_types': Posting.objects.filter(posting_type='role_type'),
+    'projects': projects,
+    'role_types': Posting.objects.filter(posting_type='role_type', published=True),
     'posting_type': posting.posting_type,
     'name': posting.name,
     'colored_icon': posting.colored_icon,
